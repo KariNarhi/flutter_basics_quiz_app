e70853b7229ca5d965ad59257ad987c97179a939
Kari Närhi <Kari.Narhi@tecinspire.com>
2021-04-03 17:43:33 +0300

    Updating Stateful Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print("Answer chosen");
+    setState(() {
+      questionIndex += 1;
+    });
+    print(questionIndex);
   }
 
   @override
@@ -20,7 +32,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text("The question!"),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text("Answer 1"),
               onPressed: answerQuestion,
