d7a6e40b804246b91eac40a84480a60b8b5c3391
Kari Närhi <Kari.Narhi@tecinspire.com>
2021-04-05 00:27:29 +0300

    New files (proper commit)

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -4,22 +4,24 @@ import './question.dart';
 import './answer.dart';
 
 class Quiz extends StatelessWidget {
- final List<Map<String, Object>> questions;
- final int questionIndex;
- final Function answerQuestion;
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  final Function answerQuestion;
 
- Quiz({@required this.questions, @required this.answerQuestion, @required this.questionIndex});
+  Quiz(
+      {@required this.questions,
+      @required this.answerQuestion,
+      @required this.questionIndex});
 
   @override
   Widget build(BuildContext context) {
     return Column(
-                children: <Widget>[
-                  Question(questions[questionIndex]['questionText']),
-                  ...(questions[questionIndex]['answers'] as List<String>)
-                      .map((answer) {
-                    return Answer(answerQuestion, answer);
-                  }).toList(),
-                ],
-              )
+      children: <Widget>[
+        Question(questions[questionIndex]['questionText']),
+        ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
+          return Answer(answerQuestion, answer);
+        }).toList(),
+      ],
+    );
   }
 }
