21d70f3b7cd6ed2faaddb95dd73f74633c65c6c3
Kari Närhi <Kari.Narhi@tecinspire.com>
2025-04-15 17:42:24 +0300

    Updated to 2025 version

--- android/app/src/main/kotlin/com/example/flutter_basics_quiz_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_basics_quiz_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,28 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/results_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/data/questions.dart';
 
-import './question.dart';
-import './answer.dart';
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
 
-class Quiz extends StatelessWidget {
-  final List<Map<String, Object>> questions;
-  final int questionIndex;
-  final Function answerQuestion;
+  @override
+  State<Quiz> createState() => _QuizState();
+}
 
-  Quiz(
-      {@required this.questions,
-      @required this.answerQuestion,
-      @required this.questionIndex});
+class _QuizState extends State<Quiz> {
+  List<String> _selectedAnswers = [];
+
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    _selectedAnswers.add(answer);
+
+    if (_selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      _selectedAnswers = [];
+      activeScreen = 'questions-screen';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Question(questions[questionIndex]['questionText']),
-        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
-            .map((answer) {
-          return Answer(() => answerQuestion(answer['score']), answer['text']);
-        }).toList(),
-      ],
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: _selectedAnswers,
+        onRestart: restartQuiz,
+      );
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
     );
   }
 }
